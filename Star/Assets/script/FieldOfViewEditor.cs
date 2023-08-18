5c1517b1d7c263a91828bff64608520eb4e575d3
jackson0731 <annf731@gmail.com>
2023-08-18 21:23:32 +0800

    敵人視野更新

--- Star/Assets/script/FieldOfViewEditor.cs
+++ Star/Assets/script/FieldOfViewEditor.cs
@@ -7,20 +7,21 @@ public class FieldOfViewEditor : Editor
     private void OnSceneGUI()
     {
         FieldOfView fov = (FieldOfView)target;
+        Vector3 fovP = fov.transform.GetChild(2).gameObject.transform.position;
         Handles.color = Color.white;
-        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.radius);
+        Handles.DrawWireArc(fovP, Vector3.up, Vector3.forward, 360, fov.radius);
 
         Vector3 viewAngle01 = DirectionFromAngle(fov.transform.eulerAngles.y, -fov.angle / 2);
         Vector3 viewAngle02 = DirectionFromAngle(fov.transform.eulerAngles.y, fov.angle / 2);
 
         Handles.color = Color.yellow;
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle01 * fov.radius);
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle02 * fov.radius);
+        Handles.DrawLine(fovP, fovP + viewAngle01 * fov.radius);
+        Handles.DrawLine(fovP, fovP + viewAngle02 * fov.radius);
 
         if (fov.canSeePlayer)
         {
             Handles.color = Color.green;
-            Handles.DrawLine(fov.transform.position, fov.playerRef.transform.position);
+            Handles.DrawLine(fovP, fov.playerRef.transform.position);
         }
     }
 
